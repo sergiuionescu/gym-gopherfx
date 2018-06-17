042a1d3a439eb58550d1c379b2ed85f3c2b30d70
Sergiu Ionescu <sergiu.ionescu@gmail.com>
2018-06-17 15:38:45 +0300

    Updated the setup

--- setup.py
+++ setup.py
@@ -1,6 +1,19 @@
-from setuptools import setup
+import setuptools
 
-setup(name='gym_gopherfx',
-      version='0.0.1',
-      install_requires=['gym']
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='gym_gopherfx',
+      version='0.1.0',
+      install_requires=['gym'],author="Sergiu Ionescu",
+      author_email="sergiu.ionescu@gmail.com",
+      description="A gym environment for reinforcement training on forex historical data.",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/sergiuionescu/gym-gopherfx",
+      packages=setuptools.find_packages(),
+      classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+),
 )
